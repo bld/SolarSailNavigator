c69cc2e77d468c932005114763ab5e7828c2a02a
Ben Diedrich <bldiedrich@gmail.com>
2015-04-30 23:02:55 -0700

    Show final orbit. Show color of orbit segments.

--- src/Controls.cs
+++ src/Controls.cs
@@ -16,100 +16,142 @@ namespace SolarSailNavigator {
 	// Duration of steering maneuver
 	public double duration;
 	public string duration_str;
+	// Days
+	double days;
+	string days_str;
 	// Warp factor
 	public double factor;
 	public string factor_str;
+	// Color
+	public Color color;
 	// Controls object this control is attached to
 	public SailControls controls;
 	// Sail this controls
-	public ModuleSolarSail sail; // Sail this control is attached to
+	public SolarSailPart sail;
 
 	// Static fields
 	public static float defaultCone = 90f;
 	public static float defaultClock = 0f;
 	public static double defaultDuration = 100000.0;
 	public static double defaultFactor = 100000.0;
-
-	// Update sail persistant variables when controls change
-	//public void Update() {
-	//    sail.cone = cone_str;
-	//    sail.clock = clock_str;
-	//    sail.duration = duration_str;
-	//    sail.factor = factor_str;
-	//}
+	public static double SecondsPerDay = 21600.0;
+	public static double SecondsPerHour = 3600.0;
+	public static double SecondsPerMinute = 60.0;
 	
-	// Tilt controls
-	
-	public void TiltPlus() {
-	    cone += 5;
-	    if (cone > 90) {
-		cone = cone - 180;
-	    }
-	}
-
-	public void TiltMinus() {
-	    cone -= 5;
-	    if (cone < -90) {
-		cone = cone + 180;
-	    }
-	}
-
-	// Rotate controls
-
-	public void RotatePlus() {
-	    clock += 5;
-	    if (clock > 180) {
-		clock = clock - 360;
-	    }
-	}
-
-	public void RotateMinus() {
-	    clock -= 5;
-	    if (clock < -180) {
-		clock = 360 + clock;
-	    }
-	}
-
-	// GUI line
-	public void GUILine () {
-	    // Line
-	    GUILayout.BeginHorizontal();
-	    // Cone
+	// Cone controls
+	public void GUICone () {
 	    GUILayout.Label(cone_str);
 	    if (GUILayout.Button("+")) {
-		TiltPlus();
+		cone += 5;
+		if (cone > 90) {
+		    cone = cone - 180;
+		}
 		cone_str = cone.ToString();
 	    }
 	    if (GUILayout.Button("-")) {
-		TiltMinus();
+		cone -= 5;
+		if (cone < -90) {
+		    cone = cone + 180;
+		}
 		cone_str = cone.ToString();
 	    }
-	    // Clock
+	}
+
+	// Clock controls
+	public void GUIClock () {
 	    GUILayout.Label(clock_str);
 	    if (GUILayout.Button("+")) {
-		RotatePlus();
+		clock += 5;
+		if (clock > 180) {
+		    clock = clock - 360;
+		}
 		clock_str = clock.ToString();
 	    }
 	    if (GUILayout.Button("-")) {
-		RotateMinus();
+		clock -= 5;
+		if (clock < -180) {
+		    clock = 360 + clock;
+		}
 		clock_str = clock.ToString();
 	    }
-	    // Duration
-	    duration_str = GUILayout.TextField(duration_str, 25);
-	    double tmpd;
-	    if (Double.TryParse(duration_str, out tmpd)) {
-		duration = tmpd;
+	}
+
+	// Time controls
+	public void GUITime () {
+
+	    // Days
+	    GUILayout.Label(days_str);
+
+	    // Increase
+	    GUILayout.BeginVertical();
+	    if (GUILayout.Button("+")) {
+		days++;
+		days_str = days.ToString();
+		duration = SecondsPerDay * days;
+		duration_str = duration.ToString();
 	    }
-	    // Warp factor
+	    if (GUILayout.Button("+10")) {
+		days += 10;
+		days_str = days.ToString();
+		duration = SecondsPerDay * days;
+		duration_str = duration.ToString();
+	    }
+	    GUILayout.EndVertical();
+
+	    // Decrease
+	    GUILayout.BeginVertical();
+	    if (GUILayout.Button("-")) {
+		if (days > 0) {
+		    days--;
+		}
+		days_str = days.ToString();
+		duration = SecondsPerDay * days;
+		duration_str = duration.ToString();
+	    }
+	    if (GUILayout.Button("-10")) {
+		if (days >= 10) {
+		    days -= 10;
+		}
+		days_str = days.ToString();
+		duration = SecondsPerDay * days;
+		duration_str = duration.ToString();
+	    }
+	    GUILayout.EndVertical();
+	}
+
+	// Warp factor controls
+	public void GUIWarp () {
 	    factor_str = GUILayout.TextField(factor_str, 25);
 	    double tmpf;
 	    if (Double.TryParse(factor_str, out tmpf)) {
 		factor = tmpf;
 	    }
-	    // End line
-	    GUILayout.EndHorizontal();
 	}
 
+	// Color controls
+	public void GUIColor (Color color) {
+	    this.color = color;
+	    var cstyle = new GUIStyle();
+	    var ctx = new Texture2D(1, 1);
+	    cstyle.normal.background = ctx;
+	    ctx.SetPixel(1,1,color);
+	    ctx.Apply();
+	    GUILayout.Label("___", cstyle);
+	}
+	
+	// GUI line
+	public void GUILine (Color color) {
+	    GUILayout.BeginHorizontal();
+
+	    GUICone();
+	    GUIClock();
+	    GUITime();
+	    GUIWarp();
+	    GUIColor(color);
+
+	    GUILayout.EndHorizontal();
+	}
+	
 	// Parse a string to a single
 	public static float ParseSingle (string str) {
 	    float tmp;
@@ -132,19 +174,24 @@ namespace SolarSailNavigator {
 	
 	// Constructor
 
-	public SailControl(ModuleSolarSail sail, float cone, float clock, double duration, double factor) {
+	public SailControl(SolarSailPart sail, float cone, float clock, double duration, double factor) {
 	    this.sail = sail;
 	    this.cone = cone;
 	    cone_str = cone.ToString();
 	    this.clock = clock;
 	    clock_str = clock.ToString();
+	    // Time
 	    this.duration = duration;
+	    this.days = Math.Floor(duration / SecondsPerDay);
+	    this.days_str = days.ToString();
+	    this.duration = days * SecondsPerDay;
 	    duration_str = duration.ToString();
+	    // Warp factor
 	    this.factor = factor;
 	    factor_str = factor.ToString();
 	}
 
-	public static SailControl Default (ModuleSolarSail sail) {
+	public static SailControl Default (SolarSailPart sail) {
 	    return new SailControl (sail, defaultCone, defaultClock, defaultDuration, defaultFactor);
 	}
     }
@@ -155,21 +202,33 @@ namespace SolarSailNavigator {
 
 	public int ncontrols;
 	public SailControl[] controls;
-	public ModuleSolarSail sail;
+	public SolarSailPart sail;
 	public double UT0;
 	public SailControl sailOff;
+	double durationTotal;
+	public Color colorFinal;
 	
 	// Static fields
+	static Color[] colorMap = { Color.yellow,
+				    Color.red,
+				    Color.green,
+				    Color.magenta,
+				    Color.grey,
+				    Color.blue,
+				    Color.white };
+
 	// Delimiter between controls
 	private char delimiter = ':';
 
 	// Constructor
 
 	// Give the sail to which this control is for
-	public SailControls (ModuleSolarSail sail) {
+	public SailControls (SolarSailPart sail) {
+
 	    // Assign sail field
 	    this.sail = sail;
 	    Debug.Log(this.sail.ToString());
+
 	    // Initial time
 	    if (sail.UT0 == 0) {
 		UT0 = Planetarium.GetUniversalTime();
@@ -177,9 +236,11 @@ namespace SolarSailNavigator {
 		UT0 = sail.UT0;
 	    }
 	    Debug.Log(UT0.ToString());
+
 	    // Off sail control
 	    sailOff = SailControl.Default(sail);
-	    // Split control strings from sail into arrays
+
+	    // If the sail doesn't have saved controls, return default
 	    if (String.IsNullOrEmpty(sail.cones) ||
 		String.IsNullOrEmpty(sail.clocks) ||
 		String.IsNullOrEmpty(sail.durations) ||
@@ -187,15 +248,21 @@ namespace SolarSailNavigator {
 		ncontrols = 1;
 		controls = new SailControl[ncontrols];
 		controls[0] = SailControl.Default(sail);
-	    } else {
+
+	    } else { // Otherwise, parse saved controls
+
+		// Split into arrays
 		var coneStrings = sail.cones.Split(delimiter);
 		var clockStrings = sail.clocks.Split(delimiter);
 		var durationStrings = sail.durations.Split(delimiter);
 		var factorStrings = sail.factors.Split(delimiter);
-		// number of controls
+
+		// Find number of controls
 		ncontrols = Math.Min(coneStrings.Length, Math.Min(clockStrings.Length, Math.Min(durationStrings.Length, factorStrings.Length)));
-		// initialize controls array
+
+		// Initialize controls array
 		controls = new SailControl[ncontrols];
+
 		// Populate controls
 		for(var i = 0; i < ncontrols; i++) {
 		    controls[i] = new SailControl(sail,
@@ -210,6 +277,7 @@ namespace SolarSailNavigator {
 	// GUI
 	public void GUI() {
 	    GUILayout.BeginVertical();
+
 	    // Set the initial time of the sequence
 	    GUILayout.BeginHorizontal();
 	    GUILayout.Label("Start time: " + UT0.ToString());
@@ -217,20 +285,53 @@ namespace SolarSailNavigator {
 		UT0 = Planetarium.GetUniversalTime();
 	    }
 	    GUILayout.EndHorizontal();
+
 	    // Controls
 	    GUILayout.BeginHorizontal();
-	    GUILayout.Label("Cone");
-	    GUILayout.Label("Clock");
-	    GUILayout.Label("Duration");
-	    GUILayout.Label("Warp");
+	    GUILayout.Label("_Cone__|");
+	    GUILayout.Label("_Clock_|");
+	    GUILayout.Label("_Days__|");
+	    GUILayout.Label("__Warp____|");
+	    GUILayout.Label("Color");
 	    GUILayout.EndHorizontal();
+
+	    int icolor = 0;
+	    durationTotal = 0.0;
 	    foreach(var control in controls) {
-		control.GUILine();
+
+		// Draw individual GUIs
+		control.GUILine(colorMap[icolor]);
+		
+		// Update total duration
+		durationTotal += control.duration;
+
+		// Update color index
+		icolor++;
+		if (icolor == colorMap.Length) {
+		    icolor = 0;
+		}
 	    }
+	    
+	    // Add/remove control segments
 	    GUILayout.BeginHorizontal();
 	    if (GUILayout.Button("Add")) { Add(); };
 	    if (GUILayout.Button("Remove")) { Remove(); };
 	    GUILayout.EndHorizontal();
+
+	    // Final orbit color
+	    colorFinal = colorMap[icolor];
+	    var cstyle = new GUIStyle();
+	    var ctx = new Texture2D(1, 1);
+	    cstyle.normal.background = ctx;
+	    ctx.SetPixel(1,1,colorFinal);
+	    ctx.Apply();
+	    GUILayout.BeginHorizontal();
+	    GUILayout.Label("Final orbit color: ");
+	    GUILayout.Label("___", cstyle);
+	    GUILayout.EndHorizontal();
+
+	    // Total duration of sequences
+	    GUILayout.Label("Total: " + durationTotal + " sec");
 	    
 	    GUILayout.EndVertical();
 	    Update();
@@ -249,7 +350,7 @@ namespace SolarSailNavigator {
 	
 	// Remove a control
 	public void Remove () {
-	    if (ncontrols > 1) {
+	    if (ncontrols > 1) { // Don't remove last control
 		var newControls = new SailControl[ncontrols - 1];
 		for (var i = 0; i < ncontrols - 1; i++) {
 		    newControls[i] = controls[i];
