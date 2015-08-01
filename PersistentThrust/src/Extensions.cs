815cdf5bb2d17f3bd740d3f43ca1b27be7c68f59
Ben Diedrich <bldiedrich@gmail.com>
2015-08-01 15:15:00 -0700

    Don't check for 0 deltaV, so that intermediate orbit is collected.

--- PersistentThrust/src/Extensions.cs
+++ PersistentThrust/src/Extensions.cs
@@ -14,25 +14,22 @@ namespace PersistentThrust {
 	// Perturb an orbit by a deltaV vector
 	public static void Perturb(this Orbit orbit, Vector3d deltaVV, double UT, double dT) {
 
-	    // If there is a deltaV, perturb orbit
-	    if (deltaVV.magnitude > 0) {
-		// Transpose deltaVV Y and Z to match orbit frame
-		Vector3d deltaVV_orbit = deltaVV.xzy;
-		Vector3d position = orbit.getRelativePositionAtUT(UT);
-		Orbit orbit2 = orbit.Clone();
-		orbit2.UpdateFromStateVectors(position, orbit.getOrbitalVelocityAtUT(UT) + deltaVV_orbit, orbit.referenceBody, UT);
-		if (!double.IsNaN(orbit2.inclination) && !double.IsNaN(orbit2.eccentricity) && !double.IsNaN(orbit2.semiMajorAxis) && orbit2.timeToAp > dT) {
-		    orbit.inclination = orbit2.inclination;
-		    orbit.eccentricity = orbit2.eccentricity;
-		    orbit.semiMajorAxis = orbit2.semiMajorAxis;
-		    orbit.LAN = orbit2.LAN;
-		    orbit.argumentOfPeriapsis = orbit2.argumentOfPeriapsis;
-		    orbit.meanAnomalyAtEpoch = orbit2.meanAnomalyAtEpoch;
-		    orbit.epoch = orbit2.epoch;
-		    orbit.referenceBody = orbit2.referenceBody;
-		    orbit.Init();
-		    orbit.UpdateFromUT(UT);
-		}
+	    // Transpose deltaVV Y and Z to match orbit frame
+	    Vector3d deltaVV_orbit = deltaVV.xzy;
+	    Vector3d position = orbit.getRelativePositionAtUT(UT);
+	    Orbit orbit2 = orbit.Clone();
+	    orbit2.UpdateFromStateVectors(position, orbit.getOrbitalVelocityAtUT(UT) + deltaVV_orbit, orbit.referenceBody, UT);
+	    if (!double.IsNaN(orbit2.inclination) && !double.IsNaN(orbit2.eccentricity) && !double.IsNaN(orbit2.semiMajorAxis) && orbit2.timeToAp > dT) {
+		orbit.inclination = orbit2.inclination;
+		orbit.eccentricity = orbit2.eccentricity;
+		orbit.semiMajorAxis = orbit2.semiMajorAxis;
+		orbit.LAN = orbit2.LAN;
+		orbit.argumentOfPeriapsis = orbit2.argumentOfPeriapsis;
+		orbit.meanAnomalyAtEpoch = orbit2.meanAnomalyAtEpoch;
+		orbit.epoch = orbit2.epoch;
+		orbit.referenceBody = orbit2.referenceBody;
+		orbit.Init();
+		orbit.UpdateFromUT(UT);
 	    }
 	}
     }
