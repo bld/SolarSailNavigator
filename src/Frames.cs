2de6ef5abe274484f1c0043dfe7d6677ba9cd5d7
Ben Diedrich <bldiedrich@gmail.com>
2015-09-13 11:44:31 -0700

    Added name to Frame class. Removed redundant Frames class.

--- src/Frames.cs
+++ src/Frames.cs
@@ -17,6 +17,7 @@ namespace SolarSailNavigator {
 	public delegate Quaternion Q (Orbit orbit, double UT, float[] angles);
 	
 	// Fields
+	public string name;
 	public string[] names;
 	public float[] defaults;
 	public Qbase qbasefn;
@@ -24,7 +25,8 @@ namespace SolarSailNavigator {
 	public Q qfn;
 	
 	// Constructor
-	public Frame (string[] names, float[] defaults, Qbase qbasefn, Qlocal qlocalfn, Q qfn) {
+	public Frame (string name, string[] names, float[] defaults, Qbase qbasefn, Qlocal qlocalfn, Q qfn) {
+	    this.name = name;
 	    this.names = names;
 	    this.defaults = defaults;
 	    this.qbasefn = qbasefn;
@@ -36,14 +38,16 @@ namespace SolarSailNavigator {
 	    {
 		// Radial/tangential/normal reference frame
 		// With Cone/Clock/Flatspin angles
-		{"RTN", new Frame(new string[] {"Cone", "Clock", "Flatspin"},
+		{"RTN", new Frame("RTN",
+				  new string[] {"Cone", "Clock", "Flatspin"},
 				  new float[] {90.0f, 0.0f, 0.0f},
 				  RTNFrame,
 				  SailFrameLocal,
 				  SailFrame)},
 		// In-track/cross-track/normal reference frame
 		// With Flight path angle (FPA)/Azimuth/Flatspin angles
-		{"ICN", new Frame(new string[] {"FPA", "Az", "Flatspin"},
+		{"ICN", new Frame("ICN",
+				  new string[] {"FPA", "Az", "Flatspin"},
 				  new float[] {0f, 0f, 0f},
 				  ICNFrame,
 				  FAFLocal,
@@ -154,61 +158,6 @@ namespace SolarSailNavigator {
 	// Total quaternion
 	public static Quaternion FAFFrame (Orbit orbit, double UT, float[] angles) {
 	    return ICNFrame(orbit, UT) * FAFLocal(angles);
-	}
-    }
-    
-    public class Frames {
-
-	// Calculate RTN frame quaternion given an orbit and UT
-	public static Quaternion RTNFrame (Orbit orbit, double UT) {
-	    // Position
-	    var r = orbit.getRelativePositionAtUT(UT).normalized.xzy;
-	    // Velocity
-	    var v = orbit.getOrbitalVelocityAtUT(UT).normalized.xzy;
-	    // Unit orbit angular momentum
-	    var h = Vector3d.Cross(r, v).normalized;
-	    // Tangential
-	    var t = Vector3d.Cross(h, r).normalized;
-	    // QRTN
-	    return Quaternion.LookRotation(t, r);
-	}
-	
-	public static Quaternion SailFrameLocal (float cone, float clock, float flatspin) {
-	    // Unit vectors (in Unity coordinate system)
-	    var r = new Vector3d(0, -1, 0); // radial
-	    //var t = new Vector3d(0, 0, 1); // tangential
-	    var n = new Vector3d(1, 0, 0); // orbit normal
-
-	    // Clock angle rotation
-	    var Qclock = Quaternion.AngleAxis(clock, r);
-	    var rclock = Qclock * r;
-	    //var tclock = Qclock * t;
-	    var nclock = Qclock * n;
-
-	    // Cone angle rotation
-	    var Qcone = Quaternion.AngleAxis(cone, nclock);
-	    var rcone = Qcone * rclock;
-	    //var tcone = Qcone * tclock;
-	    //var ncone = Qcone * nclock;
-	    
-	    // Flatspin rotation
-	    var Qfs = Quaternion.AngleAxis(flatspin, rcone);
-
-	    // Total quaternion
-	    return Qfs * Qcone * Qclock;
-	}
-
-	// Sail frame given an orbit, angles, and UT
-	public static Quaternion SailFrame (Orbit orbit, float cone, float clock, float flatspin, double UT) {
-	    var QRTN = RTNFrame(orbit, UT);
-	    var QSL = SailFrameLocal(cone, clock, flatspin);
-	    return QRTN * QSL;
-	}
-
-	public static Quaternion SailFrame (Orbit orbit, double UT, float[] angles) {
-	    var QRTN = RTNFrame(orbit, UT);
-	    var QSL = SailFrameLocal(angles[0], angles[1], angles[2]);
-	    return QRTN * QSL;
 	}
     }
 }
